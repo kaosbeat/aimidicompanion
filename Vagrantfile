8d0c565532d70f0669b2e15405197649908825b7
Kasper Jordaens <kasper.jordaens@imec.be>
2020-09-04 15:04:57 +0200

    it's a working v0.1

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@ Vagrant.configure("2") do |config|
   config.vm.synced_folder "~/aimidicompanion", "/home/vagrant/aimidicompanion"
   #config.vm.provision "shell", inline: "apt-get update"
   #config.vm.provision "shell", inline: "apt-get install -y gcc g++"
-  #config.vm.provision "shell", inline:  "apt-get install -y alsa-utils"
+  #config.vm.provision "shell", inline:  "apt-get install -y alsa-utils libsndfile1"
   #config.vm.provision "file", source: "~/aimidicompanion", destination: "~/aimidicompanion"
   #config.vm.provision "shell", inline: "bash ~/aimidicompanion/conda-install.sh", privileged: false
   #config.vm.provision "shell", inline: "bash ~/aimidicompanion/magenta-install.sh", privileged: false
--- server/server.py
+++ server/server.py
@@ -1,5 +1,66 @@
 import mido
 from mido import MidiFile
+import magenta
+# from magenta import music
+
+
+from magenta.models.melody_rnn import melody_rnn_config_flags
+from magenta.models.melody_rnn import melody_rnn_model
+from magenta.models.melody_rnn import melody_rnn_sequence_generator
+from magenta.protobuf import generator_pb2
+from magenta.protobuf import music_pb2
+# from note_seq.protobuf import music_pb2
+
+import os
+import time
+import tempfile
+import pretty_midi
+
+BUNDLE_NAME = 'attention_rnn'
+
+config = magenta.models.melody_rnn.melody_rnn_model.default_configs[BUNDLE_NAME]
+bundle_file = magenta.music.read_bundle_file(os.path.abspath(BUNDLE_NAME+'.mag'))
+
+steps_per_quarter = 4
+
+generator = melody_rnn_sequence_generator.MelodyRnnSequenceGenerator(
+      model=melody_rnn_model.MelodyRnnModel(config),
+      details=config.details,
+      steps_per_quarter=steps_per_quarter,
+      bundle=bundle_file)
+
+def _steps_to_seconds(steps, qpm):
+    return steps * 60.0 / qpm / steps_per_quarter
+
+def generate_midi(midi_data, total_seconds=10):
+    primer_sequence = magenta.music.midi_io.midi_to_sequence_proto(midi_data)
+
+    # predict the tempo
+    if len(primer_sequence.notes) > 4:
+        estimated_tempo = midi_data.estimate_tempo()
+        if estimated_tempo > 240:
+            qpm = estimated_tempo / 2
+        else:
+            qpm = estimated_tempo
+    else:
+        qpm = 120
+    primer_sequence.tempos[0].qpm = qpm
+
+    generator_options = generator_pb2.GeneratorOptions()
+    # Set the start time to begin on the next step after the last note ends.
+    last_end_time = (max(n.end_time for n in primer_sequence.notes)
+                     if primer_sequence.notes else 0)
+    generator_options.generate_sections.add(
+        start_time=last_end_time + _steps_to_seconds(1, qpm),
+        end_time=total_seconds)
+
+    # generate the output sequence
+    generated_sequence = generator.generate(primer_sequence, generator_options)
+
+    output = tempfile.NamedTemporaryFile()
+    magenta.music.midi_io.sequence_proto_to_midi_file(generated_sequence, output.name)
+    output.seek(0)
+    return output
 # from mido.sockets import PortServer, connect
 
 #
@@ -45,15 +106,3 @@ while True:
 # msg = port.receive()
 
 
-
-from mido import Message, MidiFile, MidiTrack
-
-mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
-
-track.append(Message('program_change', program=12, time=0))
-track.append(Message('note_on', note=64, velocity=64, time=32))
-track.append(Message('note_off', note=64, velocity=127, time=32))
-
-mid.save('new_song.mid')
